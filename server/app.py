b89d099af28131602d17caca1fcba3c39c924124
Kat Tannehill <katetannehill@gmail.com>
2023-11-15 21:18:28 -0700

    Complete lab

--- server/app.py
+++ server/app.py
@@ -17,7 +17,36 @@ db.init_app(app)
 api = Api(app)
 
 class Home(Resource):
-    pass
+    def get(self):
+        response_dict = {
+            'message': 'Welcome to the Newsletter RESTful API'
+        }
+        return response_dict, 200
+    
+api.add_resource(Home, '/')
+
+class Newsletters(Resource):
+    def get(self):
+        resp_dict_list = [news.to_dict() for news in Newsletter.query.all()]
+        return resp_dict_list, 200
+    
+    def post(self):
+        new_record = Newsletter(
+            title = request.form['title'],
+            body = request.form['body']
+        )
+        db.session.add(new_record)
+        db.session.commit()
+        return new_record.to_dict(), 201
+    
+api.add_resource(Newsletters, '/newsletters')
+
+class NewsletterByID(Resource):
+    def get(self, id):
+        resp_dict = db.session.get(Newsletter, id).to_dict()
+        return resp_dict, 200
+    
+api.add_resource(NewsletterByID, '/newsletters/<int:id>')
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
